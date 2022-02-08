0f252ba1b3d63646266224597bad90515fd6f64e
wis003 <wis003@ucsd.edu>
2022-02-07 18:23:27 -0800

    ALU

--- basic_proc/ALU.sv
+++ basic_proc/ALU.sv
@@ -1,43 +1,41 @@
-// Create Date:    2018.10.15
-// Module Name:    ALU 
-// Project Name:   CSE141L
-//
-// Revision 2021.07.27
-// Additional Comments: 
-//   combinational (unclocked) ALU
-import definitions::*;			          // includes package "definitions"
-module ALU #(parameter W=8, Ops=3)(
+import definitions::*;			              // includes package "definitions"
+
+// ALU module
+module ALU #(parameter W=8, Ops=4) (
   input        [W-1:0]   InputA,          // data inputs
                          InputB,
-  input        [Ops-1:0] OP,		      // ALU opcode, part of microcode
+  input        [Ops-1:0] OP,		          // ALU opcode, part of microcode
   input                  SC_in,           // shift or carry in
-  output logic [W-1:0]   Out,		      // data output 
+  output logic [W-1:0]   Out,		          // data output 
   output logic           Zero,            // output = zero flag	 !(Out)
                          Parity,          // outparity flag  ^(Out)
-                         Odd			  // output odd flag (Out[0])
-// you may provide additional status flags, if desired
-    );								    
+                         Odd			        // output odd flag (Out[0])
+  );								    
 	 
-  op_mne op_mnemonic;			          // type enum: used for convenient waveform viewing
+  op_mne op_mnemonic;			                // type enum: used for convenient waveform viewing
 	
   always_comb begin
     Out = 0;                              // No Op = default
     case(OP)							  
-      ADD : Out = InputA + InputB;        // add 
-      LSH : Out = {InputA[6:0],SC_in};    // shift left, fill in with SC_in 
-// for logical left shift, tie SC_in = 0
-	  RSH : Out = {1'b0, InputA[7:1]};    // shift right
- 	  XOR : Out = InputA ^ InputB;        // bitwise exclusive OR
+      ADD : Out = InputA + InputB;        // add
+      SUB : Out = InputA + (~InputB) + 1; // sub
       AND : Out = InputA & InputB;        // bitwise AND
-      SUB : Out = InputA + (~InputB) + 1;
+      NOR : Out = InputA ~| InputB;       // bitwise NOR
+      XOR : Out = InputA ^ InputB;        // bitwise XOR
+      LSH : Out = InputA >> InputB;       // shift left
+      RSH : Out = InputA << InputB;       // shift right
+      SEQ : Out = InputA == InputB;       // equals
+      SNE : Out = InputA != InputB;       // not equals
+      SGT : Out = InputA > InputB;        // greater than
+      SLT : Out = InputA < InputB;        // less than
     endcase
   end
 
   assign Zero   = !Out;                   // reduction NOR
   assign Parity = ^Out;                   // reduction XOR
-  assign Odd    = Out[0];				  // odd/even -- just the value of the LSB
+  assign Odd    = Out[0];				          // odd/even -- just the value of the LSB
 
   always_comb
-    op_mnemonic = op_mne'(OP);			  // displays operation name in waveform viewer
+    op_mnemonic = op_mne'(OP);			      // displays operation name in waveform viewer
 
 endmodule
--- basic_proc/Definitions.sv
+++ basic_proc/Definitions.sv
@@ -1,23 +1,26 @@
-//This file defines the parameters used in the alu
-// CSE141L
-//	Rev. 2020.5.27
+// this file defines the parameters used in the ALU
 // import package into each module that needs it
-//   packages very useful for declaring global variables
+// packages very useful for declaring global variables
 package definitions;
     
-// Instruction map
-    const logic [2:0]kADD  = 3'b000;
-    const logic [2:0]kLSH  = 3'b001;
-    const logic [2:0]kRSH  = 3'b010;
-    const logic [2:0]kXOR  = 3'b011;
-    const logic [2:0]kAND  = 3'b100;
-	const logic [2:0]kSUB  = 3'b101;
-	const logic [2:0]kCLR  = 3'b110;
-// enum names will appear in timing diagram
-    typedef enum logic[2:0] {
-        ADD, LSH, RSH, XOR,
-        AND, SUB, CLR } op_mne;
-// note: kADD is of type logic[2:0] (3-bit binary)
-//   ADD is of type enum -- equiv., but watch casting
-//   see ALU.sv for how to handle this   
+    // Instruction map
+    const logic [3:0]kADD  = 4'b0000;
+    const logic [3:0]kSUB  = 4'b0001;
+    const logic [3:0]kAND  = 4'b0010;
+    const logic [3:0]kNOR  = 4'b0011;
+    const logic [3:0]kXOR  = 4'b0100;
+	const logic [3:0]kLSH  = 4'b0101;
+	const logic [3:0]kRSH  = 4'b0110;
+    const logic [3:0]kSEQ  = 4'b0111;
+    const logic [3:0]kSNE  = 4'b1000;
+	const logic [3:0]kSGT  = 4'b1001;
+	const logic [3:0]kSLT  = 4'b1010;
+
+    // enum names will appear in timing diagram
+    typedef enum logic[3:0] {
+        ADD, SUB, AND, NOR, XOR, LSH,
+        RSH, SEQ, SNE, SGT, SLT } op_mne;
+
+// ADD is of type enum -- equiv., but watch casting
+// see ALU.sv for how to handle this   
 endpackage // definitions
