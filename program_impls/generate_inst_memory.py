cfdef5bd889416a1e5e8b7fde2ccf8c6e2e13cca
wis003 <wis003@ucsd.edu>
2022-02-23 02:04:09 -0800

    compiler done

--- program_impls/generate_inst_memory.py
+++ program_impls/generate_inst_memory.py
@@ -5,20 +5,41 @@
 # original instruction.
 # The waveform viewing tool needs a mapping of # `$value $display_string`.
 # Only want to write each unique machine code once though.
-mcodes = set()
+# mcodes = set()
 
 # Here's a lookup table for opcodes
 ops = {
-		'add': '000',
-		'load': '011',
-		'store': '110',
+		#(00)
+		'get': (0, '00000'),
+		'put': (0, '00001'),
+		'lw' : (0, '00010'),
+		'sw' : (0, '00011'),
+		'sgt': (0, '00100'),
+
+		#(01)
+		'set': (1, 0, '010'),
+		'not': (1, 1, '011'),
+
+		#(10)
+		'add'   : (2, '10000'),
+		'addi'  : (2, '10001'),
+		'sub'   : (2, '10010'),
+		'and'   : (2, '10011'),
+		'or'    : (2, '10100'),
+		'xor'   : (2, '10101'),
+		'sleft' : (2, '10110'),
+		'sright': (2, '10111'),
+
+		#(11)
+		'beq': (3, '110'),
+		'bne': (3, '111'),
 		}
 
 # This is a neat trick to catch programming errors
-TOTAL_IMEM_SIZE = 2**10
+# TOTAL_IMEM_SIZE = 2**10
 
 # Don't need to do anything fancy here
-with open('basic.asm') as ifile, open('inst_mem.hex', 'w') as imem, open('gtkwave/mcode.fmt', 'w') as wavefmt:
+with open('program1.S') as ifile, open('inst_mem.binary', 'w') as imem:
 	for lineno, line in enumerate(ifile):
 		try:
 			# Skip over blank lines, remove comments
@@ -26,31 +47,38 @@ with open('basic.asm') as ifile, open('inst_mem.hex', 'w') as imem, open('gtkwav
 			line = line.split('//')[0].strip()
 			if line == '':
 				continue
-
-			# Special-case this:
+			
+			machine_code = ''
+			# Special-case:
 			if line[:4] == 'halt':
 				machine_code = '111111111'
 			else:
-				# Every line should be of the form
-				# OP	RS RT
-				op,rs,rt = line.split()
+				curr = line.split()
+				mapping = ops[curr[0]]
 
-				# Convert 'r1' to '001'
-				rs = '{:03b}'.format(int(rs.split('r')[1]))
-				rt = '{:03b}'.format(int(rt.split('r')[1]))
+				if mapping[0] == 0:
+					machine_code = mapping[1] + '{:02b}'.format(int(curr[1].split('r')[1])) + '{:02b}'.format(int(curr[2].split('r')[1]))
 
-				# Build the instruction:
-				machine_code = ops[op] + rs + rt
+				if mapping[0] == 1:
+					if mapping[1] == 0:
+						machine_code = mapping[2] + '{:06b}'.format(int(curr[1]))
+					if mapping[1] == 1:
+						machine_code = mapping[2] + '{:02b}'.format(int(curr[1].split('r')[1])) + '1111'
+
+				if mapping[0] == 2:
+					machine_code = mapping[1] + '{:02b}'.format(int(curr[1].split('r')[1])) + '{:02b}'.format(int(curr[2].split('r')[1]))
+
+				if mapping[0] == 3:
+					machine_code = mapping[1] + '{:02b}'.format(int(curr[1].split('r')[1])) + '{:02b}'.format(int(curr[2].split('r')[1])) + '{:02b}'.format(int(curr[3].split('r')[1]))
 
 			# Write the imem entry
 			imem.write(machine_code + '\n')
-			TOTAL_IMEM_SIZE -= 1
+			# TOTAL_IMEM_SIZE -= 1
 
 			# Write out our waveform decoder
-			if machine_code not in mcodes:
-				line = line.replace('\t', ' ')
-				wavefmt.write('{} {}\n'.format(machine_code, line))
-				mcodes.add(machine_code)
+			# if machine_code not in mcodes:
+			# 	line = line.replace('\t', ' ')
+			# 	mcodes.add(machine_code)
 		except:
 			print("Error Parsing Line ", lineno)
 			print(">>>{}<<<".format(line))
@@ -59,9 +87,6 @@ with open('basic.asm') as ifile, open('inst_mem.hex', 'w') as imem, open('gtkwav
 
 	# This is a neat trick to catch programming errors:
 	# Fill the rest of instruction memory with illegal instructions.
-	#
-	wavefmt.write('xxxxxxxxx ILLEGAL!')
-	while TOTAL_IMEM_SIZE:
-		imem.write('xxxxxxxxx\n')
-		TOTAL_IMEM_SIZE -= 1
-
+	# while TOTAL_IMEM_SIZE:
+	# 	imem.write('xxxxxxxxx\n')
+	# 	TOTAL_IMEM_SIZE -= 1
