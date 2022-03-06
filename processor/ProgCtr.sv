b8e643a95bd2494401e05aee78b7fbf166f2d812
wis003 <wis003@ucsd.edu>
2022-03-06 00:32:37 -0800

    program 2 flow working

--- processor/ProgCtr.sv
+++ processor/ProgCtr.sv
@@ -9,7 +9,8 @@ module ProgCtr #(parameter L=10) (
   output logic [L-1:0] ProgCtr     	// the program counter register itself
   );
   
-  logic [1:0] startCount;
+  logic [1:0] StartCount;
+  logic start_r;
 
   // program counter can clear to 0, increment, or jump
   always_ff @(posedge Clk) begin
@@ -20,59 +21,30 @@ module ProgCtr #(parameter L=10) (
 	else
 	  ProgCtr <= ProgCtr + 1; 	       	// default increment (no need for ARM/MIPS +4, only 1 byte increments)
 
-	// override default behavior
+	// Handle the Start signal by overriding normal behavior
 	if (Reset) begin
-		ProgCtr <= 'd0;
-		startCount <= '0;
-	end else if (Start)	begin			// hold while start asserted; commence when released
-	  	if (startCount == 0)
-		  ProgCtr <= 'd0; 				// first program start hardcoded
-		else if (startCount == 1)
-		  ProgCtr <= 'd190; 			// second program start hardcoded
-		// else if (startCount == 2)
-		//   ProgCtr <= 'dXXX; 				// third program start hardcoded
-		else
-		  ProgCtr <= ProgCtr;
-		startCount <= startCount + 1;
+		StartCount <= '0;
+		start_r <= '0;
+	end 
+	else begin
+		start_r <= Start;
+		// Detect rising edge of Start
+		if ((start_r == '0) && (Start == '1)) begin
+			StartCount <= StartCount + 1'b1;
+		end
+		// Detect falling edge of Start
+		if ((start_r == '1) && (Start == '0)) begin
+			case (StartCount)
+				1: ProgCtr <= 'd000;
+				2: ProgCtr <= 'd190;
+				// 3: ProgCtr <= 'd200;
+				default: ProgCtr <= ProgCtr;
+			endcase
+		end
+		// And generally, don't let things go anywhere until first Start
+		if (StartCount == '0)
+		ProgCtr <= ProgCtr;
 	end
-	if (startCount == 0)
-		ProgCtr <= ProgCtr; // stall before first start
-
-	/****************
-	FROM STARTER CODE
-	****************/
-	// // Note about Start:
-	// //
-	// // If your programs are spread out, with a gap in your machine code listing,
-	// // you will want to make Start cause an appropriate jump.
-	// //
-	// // If your programs are packed sequentially, such that program 2 begins right
-	// // after Program 1 ends, then you won't need to do anything special here.
-
-	// // Handle the Start signal by overriding normal behavior
-	// if (Reset) begin
-	// 	StartCount <= '0;
-	// 	start_r <= '0;
-	// end else begin
-	// 	start_r <= Start;
-	// 	// Detect rising edge of Start
-	// 	if ((start_r == '0) && (Start == '1)) begin
-	// 	StartCount <= StartCount + 1'b1;
-	// 	end
-	// 	// Detect falling edge of Start
-	// 	if ((start_r == '1) && (Start == '0)) begin
-	// 	case (StartCount)
-	// 		1: ProgCtr <= 'd000;
-	// 		2: ProgCtr <= 'd100;
-	// 		3: ProgCtr <= 'd200;
-	// 		default: ProgCtr <= ProgCtr;
-	// 	endcase
-	// 	end
-	// 	// And generally, don't let things go anywhere until first Start
-	// 	if (StartCount == '0)
-	// 	ProgCtr <= ProgCtr;
-	// end
-	
   end
 endmodule
 
